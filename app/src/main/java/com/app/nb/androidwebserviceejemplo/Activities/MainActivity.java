ed0b27aae4ceaf1dc9a40b5c99438ec0926bdff1
namelessbliss <anonimo2911.2012@gmail.com>
2019-07-31 13:09:26 -0500

    Regresando a estado inicial

--- app/src/main/java/com/app/nb/androidwebserviceejemplo/Activities/MainActivity.java
+++ app/src/main/java/com/app/nb/androidwebserviceejemplo/Activities/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.app.nb.androidwebserviceejemplo.Models.City;
-import com.app.nb.androidwebserviceejemplo.Models.Town;
 import com.app.nb.androidwebserviceejemplo.R;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -20,24 +19,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        City city = null;
-
-        // JSON de ejemplo
-        String json = "{" + "id: 0" + "," +
-                " cities:[" +
-                "{ id = 1, "
-                + "name:" + " Lima" + "," +
-                " country:" + " Peru" + "}," +
-                "{ id = 2, "
-                + "name:" + " Arquipa" + "," +
-                " country:" + " Peru" + "}" +
-                "]}";
-
-        // Parseando con GSON
-
-        Gson gson = new GsonBuilder().create();
-        Town town = gson.fromJson(json, Town.class);
-
-        Toast.makeText(this, town.getCities().toString(), Toast.LENGTH_SHORT).show();
     }
 }
