756dcf028ea12fe384cb0e95715c72c709653565
namelessbliss <anonimo2911.2012@gmail.com>
2019-07-31 13:33:19 -0500

    Consumiento API, Retrofit Gson

--- app/src/main/java/com/app/nb/androidwebserviceejemplo/Activities/MainActivity.java
+++ app/src/main/java/com/app/nb/androidwebserviceejemplo/Activities/MainActivity.java
@@ -6,8 +6,13 @@ import android.widget.Toast;
 
 import com.app.nb.androidwebserviceejemplo.Models.City;
 import com.app.nb.androidwebserviceejemplo.R;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.app.nb.androidwebserviceejemplo.WeatherService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Forma nativa de parsear JSON
@@ -19,5 +24,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.openweathermap.org/data/2.5/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        WeatherService service = retrofit.create(WeatherService.class);
+
+        //Prepara la peticion
+        Call<City> cityCall = service.getCity("Lima, PE", "ffe5b339f1e33ea21f2b7dc33d773d77");
+
+        cityCall.enqueue(new Callback<City>() {
+            @Override
+            public void onResponse(Call<City> call, Response<City> response) {
+                City city = response.body();
+                Toast.makeText(MainActivity.this, city.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<City> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
